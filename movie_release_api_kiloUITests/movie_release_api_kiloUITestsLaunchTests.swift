b601e33676879861362081f293c1618a0a570710
miles.richmond24 <miles.richmond24@d155.org>
2024-01-11 18:58:32 -0600

    Initial Commit

---
+++ movie_release_api_kiloUITests/movie_release_api_kiloUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  movie_release_api_kiloUITestsLaunchTests.swift
+//  movie_release_api_kiloUITests
+//
+//  Created by Miles Richmond on 1/11/24.
+//
+
+import XCTest
+
+class movie_release_api_kiloUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
